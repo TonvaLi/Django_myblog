7862eb853cffc3e3aebee8b4071f55799526519e
TonvaLi <tt00360103@163.com>
2019-01-30 15:16:21 +0800

    新增过滤器

--- myblog/firstBlog/admin.py
+++ myblog/firstBlog/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Blog
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'content')
 
-admin.site.register(Blog)       # 将Blog应该注册到Admin中
+admin.site.register(Blog, BlogAdmin)       # 将Blog应该注册到Admin中
